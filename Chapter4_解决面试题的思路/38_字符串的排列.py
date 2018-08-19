2231d92a1fe5a4d0f153d3e52c2d8e9ae1926336
Louie Jin <jinluyuan@vip.qq.com>
2018-08-19 12:57:49 +0800

    8.19

---
+++ Chapter4_解决面试题的思路/38_字符串的排列.py
@@ -0,0 +1,23 @@
+## 题目描述：输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,
+# 则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+class Solution:
+    def __init__(self):
+        self.result = []
+        
+    def Permutation(self, ss):
+        # write code here
+        if not ss:
+            return []
+        self.perm(ss, 0)
+        return sorted(self.result)
+        
+    def perm(self, ss, begin):
+        if begin >= len(ss)-1:
+            if ss not in self.result:
+                self.result.append(ss)
+            return
+        words = list(ss)
+        self.perm(ss, begin+1)
+        for i in range(begin+1, len(words)):
+            words[begin], words[i] = words[i], words[begin]
+            self.perm(''.join(words), begin+1)
